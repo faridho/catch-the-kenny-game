c9d55de2e854231c759927f8ec0827d1cd0bc518
Faridho <faridho@zeepos.com>
2020-03-16 22:21:26 +0700

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Faridho on 16/03/20.
+//  Copyright © 2020 Faridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
